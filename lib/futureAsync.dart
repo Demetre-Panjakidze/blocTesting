2b7eb408c061d2119ff7dc6706dadfbde196349a
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2023-10-12 13:33:05 +0400

    test

--- lib/futureAsync.dart
+++
@@ -1,5 +0,0 @@
-Future<int> futureFunc(int passedVal) async {
-  await Future.delayed(const Duration(seconds: 2));
-  print(passedVal);
-  return passedVal;
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
-import 'package:bloctesting/futureAsync.dart';
-import 'package:bloctesting/streamAsync.dart';
+Stream<int> boatStream() async* {
+  for (int i = 1; i <= 10; i++) {
+    print('Sent boat no. ${i.toString()}');
+    await Future.delayed(const Duration(seconds: 2));
+    yield i;
+  }
+}
 
 void main() async {
-  await StreamFunc();
+  Stream<int> stream = boatStream();
+
+  stream.listen((event) {
+    print('Received boat no. ${event.toString()}');
+  });
 }
