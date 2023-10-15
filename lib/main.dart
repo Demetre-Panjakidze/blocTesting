7936ef883f60c15d81cbd9831418654fa99ed917
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2023-10-15 14:12:23 +0400

    counter app created

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,33 @@
+import 'package:bloctesting/counter_bloc.dart';
+import 'package:bloctesting/counter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+void main() {
+  runApp(const CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterBloc(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Text('random'),
+    );
+  }
+}
