83629a2d0b62ee50c4289c8af4cd723d1226dba1
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2023-10-25 12:22:52 +0400

    from scratch

--- lib/app.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:bloctesting/counter/counter.dart';
-
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key}) : super(home: const CounterPage());
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,1 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter/widgets.dart';
-import 'package:bloctesting/app.dart';
-import 'package:bloctesting/counter_observer.dart';
 
-void main() {
-  Bloc.observer = const CounterObserver();
-  runApp(const CounterApp());
-}
